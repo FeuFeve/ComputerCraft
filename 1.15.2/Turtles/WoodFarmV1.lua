79891eb4dc4cfad6b1ac0ac39bb1e6e40ef9ddff
FeuFeve <clement-potin@outlook.com>
2020-06-04 19:37:47 +0200

    Create WoodFarmV1.lua

---
+++ 1.15.2/Turtles/WoodFarmV1.lua
@@ -0,0 +1,22 @@
+data = inspect("up")
+print(data.name)
+print(data.metadata)
+
+
+function inspect(direction):
+	local success, data
+	
+	if direction == "up" then
+		success, data = turtle.inspectUp()
+	elseif direction == "down" then
+		success, data = turtle.inspectDown()
+	elseif direction == nil
+		success, data = turtle.inspect()
+	end
+
+	if success then
+		return data
+	else
+		return nil
+	end
+end
